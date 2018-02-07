7cfb2e163120d8e53c07498f5a2418ab8e1ddbfa
mandbjp <mandbjp+github@gmail.com>
2018-02-07 23:55:32 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-setInterval(() => {
-    console.log('Hello World!');
-}, 1000);
+const http = require('http');
+
+const data = [
+  { text: 'hoge' },
+  { text: 'fuga' }
+];
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  data.push({text: 'piyo-'+data.length });
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(data));
+
+}).listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
